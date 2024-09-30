9907ab80aa45b7b121f7d9a06ddc65d1e0b589c9
Washington Lopes <washingtonlopes2003@gmail.com>
2024-09-30 01:42:32 -0300

    Adicionada todas as funcionalidades

--- Assets/Scripts/Scripts.js
+++ Assets/Scripts/Scripts.js
@@ -88,3 +88,93 @@ function toggleClass(element) {
             break
     }
 }
+
+function smoothScroll(targetElement, duration) {
+    const headerHeight = document.querySelector('header').offsetHeight + 20;
+    const targetPosition = targetElement.getBoundingClientRect().top - headerHeight;
+    const startPosition = window.pageYOffset;
+    let startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, targetPosition, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+function contato() {
+    const options = document.querySelectorAll('.options-form > div')
+
+    options.forEach(opt => {
+      opt.classList.remove('opt-selection')
+    })
+
+    document.querySelector("#contato").classList.add('opt-selection')
+
+    const form1 = document.querySelector('.form-contato')
+    const form2 = document.querySelector('.form-orcamento')
+    const form3 = document.querySelector('.form-locacao')
+    const form4 = document.querySelector('.form-trabalhe-conosco')
+
+    form1.classList.remove('form-selection')
+    form2.classList.add('form-selection')
+    form3.classList.add('form-selection')
+    form4.classList.add('form-selection')
+
+    smoothScroll(document.getElementById('contacts'), 2000);
+}
+
+function orcamento() {
+    const options = document.querySelectorAll('.options-form > div')
+
+    options.forEach(opt => {
+      opt.classList.remove('opt-selection')
+    })
+
+    document.querySelector("#orcamento").classList.add('opt-selection')
+
+    const form1 = document.querySelector('.form-contato')
+    const form2 = document.querySelector('.form-orcamento')
+    const form3 = document.querySelector('.form-locacao')
+    const form4 = document.querySelector('.form-trabalhe-conosco')
+
+    form1.classList.add('form-selection')
+    form2.classList.remove('form-selection')
+    form3.classList.add('form-selection')
+    form4.classList.add('form-selection')
+
+    smoothScroll(document.getElementById('contacts'), 2000);
+}
+
+function locacao() {
+    const options = document.querySelectorAll('.options-form > div')
+
+    options.forEach(opt => {
+      opt.classList.remove('opt-selection')
+    })
+
+    document.querySelector("#locacao").classList.add('opt-selection')
+
+    const form1 = document.querySelector('.form-contato')
+    const form2 = document.querySelector('.form-orcamento')
+    const form3 = document.querySelector('.form-locacao')
+    const form4 = document.querySelector('.form-trabalhe-conosco')
+
+    form1.classList.add('form-selection')
+    form2.classList.add('form-selection')
+    form3.classList.remove('form-selection')
+    form4.classList.add('form-selection')
+
+    smoothScroll(document.getElementById('contacts'), 2000);
+}
