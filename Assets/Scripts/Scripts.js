86f48dbee06ab5d63f6277f0366f04ea6c0ec4fe
Washington Lopes <washingtonlopes2003@gmail.com>
2024-09-29 23:51:52 -0300

    Inclui a section de obras

--- Assets/Scripts/Scripts.js
+++ Assets/Scripts/Scripts.js
@@ -6,43 +6,43 @@ function toggleAccordion(accordion) {
     arrow.classList.toggle('icon-arrow-active')
 }
 
-let currentIndex = 0;
+let currentIndex = 0
 
 function showSlides() {
-    const items = document.querySelectorAll('.item');
-    const totalItems = items.length;
+    const items = document.querySelectorAll('.item')
+    const totalItems = items.length
 
     items.forEach((item, index) => {
-        item.style.display = (index >= currentIndex && index < currentIndex + 3) ? 'block' : 'none';
+        item.style.display = (index >= currentIndex && index < currentIndex + 3) ? 'block' : 'none'
     });
 
     if (currentIndex === totalItems - 3) {
-        document.querySelector('.next').disabled = true;
+        document.querySelector('.next').disabled = true
     } else {
-        document.querySelector('.next').disabled = false;
+        document.querySelector('.next').disabled = false
     }
 
     if (currentIndex === 0) {
-        document.querySelector('.prev').disabled = true;
+        document.querySelector('.prev').disabled = true
     } else {
-        document.querySelector('.prev').disabled = false;
+        document.querySelector('.prev').disabled = false
     }
 }
 
 function nextSlide() {
-    const items = document.querySelectorAll('.item');
-    const totalItems = items.length;
+    const items = document.querySelectorAll('.item')
+    const totalItems = items.length
 
     if (currentIndex < totalItems - 3) {
-        currentIndex += 1;
-        showSlides();
+        currentIndex += 1
+        showSlides()
     }
 }
 
 function prevSlide() {
     if (currentIndex > 0) {
-        currentIndex -= 1;
-        showSlides();
+        currentIndex -= 1
+        showSlides()
     }
 }
 
@@ -67,24 +67,24 @@ function toggleClass(element) {
             form2.classList.add('form-selection')
             form3.classList.add('form-selection')
             form4.classList.add('form-selection')
-            break;
+            break
         case 'orcamento':
             form1.classList.add('form-selection')
             form2.classList.remove('form-selection')
             form3.classList.add('form-selection')
             form4.classList.add('form-selection')
-            break;
+            break
         case 'locacao':
             form1.classList.add('form-selection')
             form2.classList.add('form-selection')
             form3.classList.remove('form-selection')
             form4.classList.add('form-selection')
-            break;
+            break
         case 'trabalhe-conosco':
             form1.classList.add('form-selection')
             form2.classList.add('form-selection')
             form3.classList.add('form-selection')
             form4.classList.remove('form-selection')
-            break;
+            break
     }
 }
